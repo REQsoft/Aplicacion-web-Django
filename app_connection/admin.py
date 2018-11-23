fb61d3676322480adfb0c9443dea162078330400
John Mario Getial <jmariogb2803@gmail.com>
2018-11-23 10:16:04 -0500

    Borrado todo

--- app_connection/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Connection
-
-# Register your models here.
-admin.site.register(Connection)
--- main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Group, Authentication
-
-admin.site.register(Group)
-admin.site.register(Authentication)
-
-# Register your models here.
